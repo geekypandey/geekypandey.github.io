2bd06947f9c2d605a3936e3586d5eeb9fc3ffed4
Anurag Pandey <anuragcarbon1998@gmail.com>
2021-07-21 16:28:33 +0530

    New post on cf-template and added auto-publish script
    Auto-publish script for publishing the articles on dev.to using their
    api's

---
+++ _posts/2021-07-21-cp-template.markdown
@@ -0,0 +1,21 @@
+---
+layout: post
+title:  "CP Template"
+date:   2021-07-21 +0530
+categories: competitive-programming
+---
+### Disclaimer:
+> I am a beginner in competitive programming, and this is me documenting my learnings. So, anything I say is just what I think at the moment and should not be taken as hard truths.
+
+## My CP Template
+
+It is always fun to write and maintain your own template file for competitive programming, just like maintaining `.virmc` file.
+I primarily program in C++. So here is my cpp template file:
+
+{% gist aebf1361149957fe0b9b3d9eddd0d1d9 %}
+
+Now how to keep improving this template,
+1. Experiment with different things, and make it customize to your needs.
+2. Look at other people template and learn.
+
+The same thing could be said for editing your `.vimrc` file, that is if you use vim :-)
---
+++ scripts/publish_to_dev.py
@@ -0,0 +1,57 @@
+# Task
+# To automatically publish the article on dev.to
+# Get all the articles: Check the names, and publish the ones that isn't published yet.
+# Parsing the articles in _post
+import os
+import re
+from pathlib import Path
+
+import frontmatter
+import requests
+
+def get_articles_names():
+    username = 'geekypandey'
+    url = f'https://dev.to/api/articles?username={username}'
+    res = requests.get(url)
+    if res.status_code != 200:
+        exit(1)
+    articles = res.json()
+    articles = [article.get('title') for article in articles]
+    return articles
+
+
+if __name__ == '__main__':
+    articles = get_articles_names()
+    posts = Path('../_posts')
+    dev_url = 'https://dev.to/api/articles'
+    username = 'geekypandey'
+    gist = f'https://gist.github.com/{username}'
+    api_key = os.environ.get('API_KEY')
+    headers = {
+        'Content-Type': 'application/json',
+        'api-key': api_key
+    }
+
+    for post in posts.iterdir():
+        if not post.is_file():
+            continue
+        p = frontmatter.load(post)
+        title = p.get('title')
+        if title not in articles:
+            # post that article
+            metadata = {
+                'title': p.metadata.get('title'),
+                'series': 'Competitive Programming',
+                'published': True
+            }
+            p.metadata = metadata
+            content = frontmatter.dumps(p)
+            content = re.sub(r'(gist) (\w+)', rf'\1 {gist}/\2', content)
+            article = {}
+            article['article'] = {}
+            article['article']['body_markdown'] = content
+            res = requests.post(dev_url, json=article, headers=headers)
+            if res.status_code == 201:
+                print(f'{title} published successfully!')
+            else:
+                print(f'{title} not published! Error: {res.status_code}')
